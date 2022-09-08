6192f552050b787bbc514cd71f81e4e7319e2435
Nazire <nazirej@gmail.com>
2022-09-08 05:03:27 -0400

    commit tree 6c04ef068bf6f809cb375aceee2aca293af0ad5e

---
+++ index.js
@@ -0,0 +1,12 @@
+function divide (div) {
+    let a = 2000
+    let b = 100 
+    return a / b
+}
+const square = (parameter1) => parameter1 * parameter1;
+//square(2,2);
+const add = (parameter1, parameter2) => {
+    console.log(`add ${parameter1}`);
+    console.log(`add ${parameter2}`);
+    return parameter1 + parameter2;
+} 
